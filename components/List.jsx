31a32fa66ccacbb61f8a2887e6867d54e7d4cb8c
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-22 17:17:37 +0200

    [UPD] mileston 2 completata

--- components/List.jsx
+++ components/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useState } from 'react'
 
 const List = () => {
 
@@ -9,14 +10,38 @@ const List = () => {
         { name: 'Latte', price: 1.0 },
         { name: 'Pasta', price: 0.7 },
     ];
+    // lista prodotti del carrello
+    const [addedProducts, setAddedProducts] = useState([]);
+
+    // funzione per aggiungere i prodotti al carrello
+    const addToCart = product => {
+        const aggiunto = addedProducts.some(p => p.name === product.name)
+        if (aggiunto) {
+            return;
+        }
+        setAddedProducts(curr => [...curr, { ...product, quantity: 1 }])
+    }
+
     return (
         <div>
             <ul>
                 {
                     // genero la lista dei prodotti con map
-                    products.map((p) => {
-                        return <li>
-                            <p><b>Prodotto:</b> {p.name} - <b>Prezzo:</b> {p.price} &euro;</p>
+                    products.map((p, i) => {
+                        return <li key={i}>
+                            <p><b>Prodotto:</b> {p.name} - <b>Prezzo:</b> {p.price} &euro; <button onClick={() => addToCart(p)} >Aggiungi al carrello</button></p>
+                        </li>
+                    })
+                }
+            </ul>
+
+            <h4>Prodotti nel carrello</h4>
+            <ul>
+                {
+                    // genero la lista dei prodotti nel carrello 
+                    addedProducts.map((a, i) => {
+                        return <li key={i}>
+                            <p><b>{a.quantity} x Prodotto:</b> {a.name} - <b>Prezzo:</b> {a.price} &euro;</p>
                         </li>
                     })
                 }
